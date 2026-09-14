6f18a76640db5322be74a12cef9ab19173af43c7
Ratin Gao <ratin@knsoft.org>
2026-09-15 01:20:50 +0800

    [SYS] Add registry creation and value writing helpers

--- Source/KNSoft.MakeLifeEasier/System/Registry.h
+++ Source/KNSoft.MakeLifeEasier/System/Registry.h
@@ -33,6 +33,59 @@ Sys_RegOpenKey(
     return Sys_RegOpenKeyEx(KeyHandle, NULL, DesiredAccess, Path);
 }
 
+FORCEINLINE
+NTSTATUS
+Sys_RegCreateKeyEx(
+    _Out_ PHANDLE KeyHandle,
+    _In_opt_ HANDLE RootKey,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ PCUNICODE_STRING Path,
+    _In_ ULONG CreateOptions,
+    _Out_opt_ PULONG Disposition)
+{
+    OBJECT_ATTRIBUTES ObjectAttributes;
+
+    InitializeObjectAttributes(&ObjectAttributes,
+                               (PUNICODE_STRING)Path,
+                               OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE,
+                               RootKey,
+                               NULL);
+
+    return NtCreateKey(KeyHandle, DesiredAccess, &ObjectAttributes, 0, NULL, CreateOptions, Disposition);
+}
+
+FORCEINLINE
+NTSTATUS
+Sys_RegCreateKey(
+    _Out_ PHANDLE KeyHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ PCUNICODE_STRING Path)
+{
+    return Sys_RegCreateKeyEx(KeyHandle, NULL, DesiredAccess, Path, REG_OPTION_NON_VOLATILE, NULL);
+}
+
+FORCEINLINE
+NTSTATUS
+Sys_RegSetData(
+    _In_ HANDLE KeyHandle,
+    _In_ PCUNICODE_STRING ValueName,
+    _In_ ULONG Type,
+    _In_reads_bytes_(DataSize) LPCVOID Data,
+    _In_ ULONG DataSize)
+{
+    return NtSetValueKey(KeyHandle, (PUNICODE_STRING)ValueName, 0, Type, (PVOID)Data, DataSize);
+}
+
+FORCEINLINE
+NTSTATUS
+Sys_RegSetDword(
+    _In_ HANDLE KeyHandle,
+    _In_ PCUNICODE_STRING ValueName,
+    _In_ DWORD Value)
+{
+    return Sys_RegSetData(KeyHandle, ValueName, REG_DWORD, &Value, sizeof(Value));
+}
+
 #define SYS_REG_DEFINE_VALUE_DATA(VarName, ValueSize) DEFINE_ANYSIZE_STRUCT(VarName, KEY_VALUE_PARTIAL_INFORMATION, UCHAR, ValueSize)
 #define SYS_REG_VALUE_DATA_SIZE(ValueSize) (UFIELD_OFFSET(KEY_VALUE_PARTIAL_INFORMATION, Data) + (ValueSize))
 
